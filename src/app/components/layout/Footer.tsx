9b872ab67aba1b73f05f46de08611315b499826c
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-08-28 23:18:36 -0300

    fix

--- src/app/components/layout/Footer.tsx
+++ src/app/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <footer className='flex-col bg-primary-600 p-2 w-full mx-auto rounded items-center'>
       <SocialIcons />
-      <p className='text-xs'>Footer</p>
+      <p className='text-xs'>Footer22</p>
     </footer>
   )
 }
