5670e289bd3d64d6e38de7ef11e89aaeb34845e2
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-09-21 22:42:52 -0300

    code resume component

---
+++ src/app/components/sections/ResumeHome.tsx
@@ -0,0 +1,14 @@
+import { Section } from '../elements/Section'
+
+export const ResumeHome = () => {
+  return (
+    <Section>
+      <h1>Reginaldo Gomes</h1>
+      <p>
+        Profissional com experiência em desenvolvimento Front-end e com
+        experiência nas linguagens JavaScript e TypeScript e é especializado na
+        biblioteca ReatcJS e no framework NextJS.
+      </p>
+    </Section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,13 @@
 // import Image from "next/image";
 
 import { Container } from './components/elements/Container'
+import { ResumeHome } from './components/sections/ResumeHome'
 import { Skills } from './components/sections/Skills'
 
 export default function Home() {
   return (
     <Container>
-      <h1>front-end developer</h1>
-      <p>
-        Profissional com experiência em desenvolvimento Front-end e com
-        experiência nas linguagens JavaScript e TypeScript e é especializado na
-        biblioteca ReatcJS e no framework NextJS. 22
-      </p>
+      <ResumeHome />
       <Skills />
     </Container>
   )
