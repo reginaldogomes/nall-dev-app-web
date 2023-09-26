72829e73560bd48253db72161b6c6465448bdbea
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-09-25 23:54:30 -0300

    resume home

--- src/app/components/sections/ResumeHome.tsx
+++ src/app/components/sections/ResumeHome.tsx
@@ -1,13 +1,17 @@
 import { Section } from '../elements/Section'
 
+const avatar = .../na
+
 export const ResumeHome = () => {
   return (
     <Section>
-      <h1>Reginaldo Gomes</h1>
+      <Image src={} />
       <p>
-        Profissional com experiência em desenvolvimento Front-end e com
-        experiência nas linguagens JavaScript e TypeScript e é especializado na
-        biblioteca ReatcJS e no framework NextJS.
+        Tenho uma sólida bagagem com mais de 10 anos de experiência no
+        desenvolvimento de produtos digitais, com um foco especial em soluções
+        para plataformas web e mobile. Minha trajetória profissional é um
+        testemunho vivo de aprendizados contínuos e de uma paixão inabalável por
+        transformar ideias em realidade digital.
       </p>
     </Section>
   )
