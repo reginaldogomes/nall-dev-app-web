d4a5b3111f441658383d0dfd7bbac9393d135dfd
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-09-27 13:54:23 -0300

    home code

--- src/app/components/sections/ResumeHome.tsx
+++ src/app/components/sections/ResumeHome.tsx
@@ -5,8 +5,8 @@ const avatar = '/nall-resume.svg'
 
 export const ResumeHome = () => {
   return (
-    <section className='mx-auto flex-col items-center gap-x-9 py-4 px-8 rounded text-center'>
-      <div className='mx-auto text-center'>
+    <section className='grid grid-cols-1 gap-y-4 items-center mx-auto py-4 px-4 rounded'>
+      <div className='mx-auto'>
         <Image
           src={avatar}
           width={100}
@@ -14,13 +14,15 @@ export const ResumeHome = () => {
           alt='Picture of the author'
         />
       </div>
-      <p>
+      <p className='text-center'>
         Com mais de 10 anos de experiência no desenvolvimento de produtos
         digitais, especialmente para plataformas web e mobile, minha jornada
         profissional é um reflexo contínuo de aprendizados e da paixão em tornar
         ideias em realidade digital.
       </p>
-      <LinkUrl href='/about'>Saiba Mais</LinkUrl>
+      <div className='mx-auto'>
+        <LinkUrl href='/about'>Saiba Mais</LinkUrl>
+      </div>
     </section>
   )
 }
