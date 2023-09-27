513c45a89b5454430641d0e5a817a03dab060e09
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-09-27 14:44:05 -0300

    fix link

--- src/app/components/elements/LinkUrl.tsx
+++ src/app/components/elements/LinkUrl.tsx
@@ -9,7 +9,7 @@ const LinkUrl = ({ href, children }: LinkProps) => {
   return (
     <Link
       href={href}
-      className='bg-ascent-600 rounded p-2 min-w-max text-ascent-100 uppercase text-sm'
+      className='bg-ascent-600 rounded p-2 text-ascent-100 uppercase text-sm'
     >
       {children}
     </Link>
