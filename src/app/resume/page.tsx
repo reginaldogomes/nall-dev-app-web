e614344adeedb5c17f7ab9810f392d7e657b859f
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2023-10-06 07:10:34 -0300

    resume mobile data

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -4,18 +4,18 @@ import Title from '../components/elements/Title'
 const desenvolvedor = {
   experiencia: [
     {
-      cargo: 'Desenvolvedor Full Stack',
-      empresa: 'Tech Solutions Ltda',
+      cargo: 'Desenvolvedor Front-end Web',
+      empresa: 'BH Solutions',
       periodo: 'Janeiro de 2018 - Presente',
       descricao:
         'Trabalhei como desenvolvedor full stack, participando no desenvolvimento de aplicações web utilizando tecnologias como HTML, CSS, JavaScript, Node.js e React. tecnologias como HTML, CSS, JavaScript, Node.js e React.',
     },
     {
-      cargo: 'Desenvolvedor Front-end',
-      empresa: 'CodeMaster Studio',
-      periodo: 'Julho de 2015 - Dezembro de 2017',
+      cargo: 'Desenvolvedor Front-end Web',
+      empresa: 'Locaweb',
+      periodo: 'Abril de 2015 - Dezembro de 2017',
       descricao:
-        'Fui responsável pelo desenvolvimento e manutenção de interfaces de usuário utilizando HTML, CSS e JavaScript. Colaborei com a equipe de design para criar experiências de usuário intuitivas e atraentes.',
+        'Fui responsável pelo desenvolvimento e manutenção das interfaces de usuário utilizando HTML, CSS e JavaScript no site de comércio eletrônico da Locaweb. Também atuei com o deploy da aplicação em ambiente Azure.',
     },
     {
       cargo: 'Estagiário de Desenvolvimento',
@@ -31,7 +31,7 @@ export default function Page() {
   return (
     <Section>
       <Title text='Experiência Profissional' />
-      <div className='grid grid-cols-2 sm:grid-cols-1 gap-2'>
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-2'>
         {desenvolvedor.experiencia.map((exp, index) => (
           <div key={index} className='grid gap-y-px bg-primary-500 p-4 rounded'>
             <h2>{exp.cargo}</h2>
