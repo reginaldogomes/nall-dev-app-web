7afd7a050f5dc9bbe9110f7ebfc56eb9f494dc73
Reginaldo Gomes <reginaldo@bhsolutions.com.br>
2024-04-18 23:26:27 -0300

    refactored code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 // import PostList from './components/PostList'
-import { Container } from '../components/elements/Container'
-import { ResumeHome } from '../components/sections/ResumeHome'
+import { Container } from '@/components/elements/Container'
+import { ResumeHome } from '@/components/sections/ResumeHome'
 // import { Service } from './components/sections/Service'
 
+import { usePosts } from '@/context/PostsContext'
+
 export default function Home() {
+  const { posts } = usePosts()
+
   return (
     <Container>
       <ResumeHome />
-      {/* <Service services={services} /> */}
-      {/* <PostList /> */}
+      <div>
+        {posts &&
+          posts.map((post) => (
+            <div key={post.id}>
+              <h2>{post.title}</h2>
+              <p>{post.body}</p>
+            </div>
+          ))}
+      </div>
     </Container>
   )
 }
